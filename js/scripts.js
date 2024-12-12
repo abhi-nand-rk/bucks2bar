1ec467a250d2cfb309a97fe4900a71d52e58e2f8
abhi-nand-rk <116075251+abhi-nand-rk@users.noreply.github.com>
2024-12-12 23:10:10 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@
 document.getElementById('username').addEventListener('keyup', function () {
     var username = document.getElementById('username').value;
     // regex to check if username has at least 1 capital letter, 1 special character, 1 number and is at least 8 characters long
-    var regex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    var regex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*~])(?=.{8,})/;
     if (regex.test(username)) {
         document.getElementById('username').style.borderColor = 'green';
     } else {
